84d7bf76783ded0c6e95f0138033346fbe1bae7d
Gaurav kumar <gaurav2146.gk@gmail.com>
2021-11-27 20:36:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { FeedComponent } from './feed/feed.component';
+import { HeaderComponent } from './feed/header/header.component';
+import { TweetBoxComponent } from './feed/tweet-box/tweet-box.component';
+import { PostComponent } from './feed/post/post.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FeedComponent,
+    HeaderComponent,
+    TweetBoxComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
